2623ff8362628be3d4e06d13d4e4ed2a52c8ea9b
Piyush <piyush4for@gmail.com>
2021-09-02 15:22:19 +0530

    with factory contract

--- blueSmith.sol
+++ blueSmith.sol
@@ -1,11 +1,29 @@
 pragma solidity ^0.4.17;
 
+contract Factory {
+    address[] public ConnectedUsers;
+        function StoringAdress (string USERNAME, string PASSWORD) public {
+                address newUsers = address (new BlueSmith( USERNAME, PASSWORD));
+                ConnectedUsers.push(newUsers);
+        }
+        
+        function getMoney (uint money) private {
+            address ownerbhaiya = 0x4C5C34262C543e10C79a1160D5D974375486C62a;
+            require(msg.sender == ownerbhaiya);
+            ownerbhaiya.transfer(money);
+        }
+        
+        function getUsers() public view returns (address []) {
+            return ConnectedUsers;
+        }
+}
+
 contract BlueSmith {
     string public userName;
     string public userPassword;
     address[] public members;
     mapping(address => bool) IsHeMember;
-    mapping(string => uint) tokens;
+    mapping(string => uint) public tokens;
     
     function BlueSmith(string USERNAME, string PASSWORD) public {
         userName = USERNAME;
@@ -50,6 +68,7 @@ contract BlueSmith {
         tokens[getToken()] = msg.value;
     }
     
+
     function getTokenValue(string input) public view returns (uint){
         require(IsHeMember[msg.sender]);
         return tokens[string(input)];
